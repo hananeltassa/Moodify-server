64ab2b5208c22e6dfa6da90adb06de7f0aaa6b82
hananeltassa <hananeltassa15@gmail.com>
2024-12-15 22:55:22 +0200

    Created authMiddleware

---
+++ src/middlewares/roleMiddleware.js
@@ -0,0 +1,12 @@
+export const checkRole = (roles) => {
+    return (req, res, next) => {
+      const userRole = req.user.role; 
+  
+      if (!roles.includes(userRole)) {
+        return res.status(403).json({ error: "Access forbidden: Insufficient permissions" });
+      }
+  
+      next(); 
+    };
+};
+  
