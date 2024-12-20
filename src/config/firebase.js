b895751906de2b90e5ad7e252c49e6e6c3ecfac8
hananeltassa <hananeltassa15@gmail.com>
2024-12-20 23:33:48 +0200

    Updated configrations

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../../moodify-12-firebase-adminsdk-vof02-d7862b8c2f.json' assert { type: 'json' };
+import fs from 'fs';
+const serviceAccount = JSON.parse(fs.readFileSync(new URL('../../moodify-12-firebase-adminsdk-vof02-d7862b8c2f.json', import.meta.url)));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
