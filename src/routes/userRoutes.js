9c197ac131759eaa3b90f373ffdb6a244355cb12
hananeltassa <hananeltassa15@gmail.com>
2024-12-30 23:33:48 +0200

    Updated Routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { authenticate } from '../middlewares/authMiddleware.js';
-import { registerUser, loginUser, googleLogin , getProfile, updateProfile, changePassword} from "../controllers/userController.js";
+import { registerUser, loginUser , getProfile, updateProfile, changePassword} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/google-login", googleLogin);
 
 router.put("/change-password", authenticate, changePassword);
 router.get("/profile", authenticate, getProfile); 
