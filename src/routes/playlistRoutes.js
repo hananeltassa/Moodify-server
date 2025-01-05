574154dcd12bd165f1acd62d7f3f3604be38d02e
hananeltassa <hananeltassa15@gmail.com>
2025-01-05 19:54:00 +0200

    Updated playlist routes

--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -1,19 +1,19 @@
 import express from "express";
 import {
   createPlaylist,
+  getUserPlaylists,
+  addSongToPlaylist,
+  getPlaylistSongs,
+  deleteSongFromPlaylist,
 } from "../controllers/playlistController.js";
+import { authenticateUser } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-
-router.post("/", createPlaylist);
-
-router.get("/:userId", getUserPlaylists);
-
-router.post("/:playlistId/songs", addSongToPlaylist);
-
-router.get("/:playlistId/songs", getPlaylistSongs);
-
-router.delete("/:playlistId/songs/:songId", deleteSongFromPlaylist);
+router.post("/", authenticateUser, createPlaylist);
+router.get("/:userId", authenticateUser, getUserPlaylists);
+router.post("/:playlistId/songs", authenticateUser, addSongToPlaylist);
+router.get("/:playlistId/songs", authenticateUser, getPlaylistSongs);
+router.delete("/:playlistId/songs/:songId", authenticateUser, deleteSongFromPlaylist);
 
 export default router;
