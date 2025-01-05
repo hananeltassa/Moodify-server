32e401a0d3970b59c8ed1dc5db382ed5ef233e6b
hananeltassa <hananeltassa15@gmail.com>
2025-01-05 19:45:42 +0200

    Added playlist routes

--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -5,6 +5,15 @@ import {
 
 const router = express.Router();
 
+
 router.post("/", createPlaylist);
+
+router.get("/:userId", getUserPlaylists);
+
+router.post("/:playlistId/songs", addSongToPlaylist);
+
+router.get("/:playlistId/songs", getPlaylistSongs);
+
+router.delete("/:playlistId/songs/:songId", deleteSongFromPlaylist);
 
 export default router;
