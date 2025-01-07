4b91abeaa93874d7c612349a08ca735452097a8a
hananeltassa <hananeltassa15@gmail.com>
2025-01-07 22:01:47 +0200

    Updated aiCoachRoutes

--- src/routes/aiCoachRoutes.js
+++ src/routes/aiCoachRoutes.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import { authenticate } from '../middlewares/authMiddleware.js';
-import { createChallenge, updateChallengeStatus } from '../controllers/aiCoachController.js';
+import {
+  createChallenge,
+  updateChallengeStatus,
+  getChallenges,
+  getChallengeById,
+  deleteChallenge,
+  generateDailyChallenges,
+  getUserStats,
+} from '../controllers/aiCoachController.js';
 
 const router = express.Router();
 
 router.post('/create-challenge', authenticate, createChallenge);
 router.put('/challenges/:id/status', authenticate, updateChallengeStatus);
+router.get('/challenges', authenticate, getChallenges);
+router.get('/challenges/:id', authenticate, getChallengeById);
+router.delete('/challenges/:id', authenticate, deleteChallenge);
+router.post('/challenges/daily', authenticate, generateDailyChallenges);
+router.get('/challenges/stats', authenticate, getUserStats);
 
 export default router;
