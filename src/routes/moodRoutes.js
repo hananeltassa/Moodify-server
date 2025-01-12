f00a41cfacc48294cdb6905532cff4571ed5df94
hananeltassa <hananeltassa15@gmail.com>
2025-01-12 17:10:43 +0200

    Added moodRoutes

--- src/routes/moodRoutes.js
+++ src/routes/moodRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { authenticate } from '../middlewares/authMiddleware.js';
-import { textDetectedMood } from "../controllers/moodController.js";
+import { textDetectedMood, handleAudioUpload } from "../controllers/moodController.js";
+import { uploadMiddleware } from "../middlewares/uploadMiddleware.js";
 
 const router = express.Router();
 
 router.post('/text-mood', authenticate, textDetectedMood);
+router.post("/voice-mood", uploadMiddleware, handleAudioUpload);
 
 export default router;
